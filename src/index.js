dcbb10acc0ccc4d29cb870351fa0ab6d8c0f4d6d
maylenepoulsen <mayleneski@hotmail.com>
2020-06-16 18:00:17 -0500

    added first route and tested with Postman

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 require('./db/mongoose')
 const express = require('express')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT
+
+app.use(express.json())
+app.use(userRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
---
+++ src/routers/user.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const User = require('../models/user')
+
+const router = new express.Router()
+
+router.post('/users', async (req, res) => {
+  const user = new User(req.body)
+
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (e) {
+    res.send(400).send(e)
+  }
+})
+
+module.exports = router
